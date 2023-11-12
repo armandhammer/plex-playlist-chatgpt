31c4dce52042647d60388174b4881dc9b6d36b71
bengarland <46571303+bengarland@users.noreply.github.com>
2023-11-12 13:54:48 -0700

    Update plex-playlist-chatgpt-prompt-user.py

--- plex-playlist-chatgpt-prompt-user.py
+++ plex-playlist-chatgpt-prompt-user.py
@@ -6,17 +6,13 @@
 # 2) it prompts for a name for the new playlist
 # 3) it reads a text file which has a list of songs in the format ARTIST - TRACK
 # with one song per line. It then searches the local Plex library for matching tracks
-# and if those tracks are found, it adds them to the playlist. It then searches for
+# and if those tracks are found, it adds them to the playlist. If no local tracks are found,
+# it uses the defined PLACEHOLDER_ARTIST and PLACEHOLDER_TRACK in order to create the 
+# playlist (it deletes this track later, before adding it to Plex). It then searches for
 # missing tracks on Tidal, then appends those tracks to the playlist. During the search
 # it excludes results which the track or album name includes a date format, e.g.
 # YYYY-MM-DD or the words 'live', 'concert', or 'sbd' because I don't want live versions
 # of songs in my playlists.
-# 4) there's fuzzy matching of the tracks with an 85% default. Change FUZZ_AMT if you're
-# not getting the expected results.
-
-# If you want to add the playlist to an admin account and not prompt for a user, see 
-# the revised main() function posted as a separate file in the repo, and cut-and-paste
-# that code in place of the main() at the end of this script.
 
 
 from plexapi.server import PlexServer
@@ -35,9 +31,13 @@ PLEX_URL = 'http://XXX.XXX.X.XXX:32400'  # Enter your server IP
 PLEX_TOKEN = '[YOUR TOKEN]'
 SECTION_TITLE = 'Music'     # The name of your music library
 FILENAME = 'playlist.txt'   # The text file with the list of ARTIST - TRACK
-ADMIN_NAME = '[ADMIN NAME]'
-ADMIN_PASS = '[ADMIN PASSWORD]'
-FUZZ_AMT = 85               # Amount of fuzzy track matching
+ADMIN_NAME = '[ADMIN NAME]' # Enter your admin name in single quotes
+ADMIN_PASS = '[ADMIN PASS]' # Enter your admin password in single quotes
+FUZZ_AMT = 50               # Amount of fuzzy track matching
+
+BATCH_SIZE = 20
+PLACEHOLDER_ARTIST = 'LCD Soundsystem'    #edit with an artist and track in your local Plex library
+PLACEHOLDER_TITLE = 'Dance Yrself Clean'
 
 
 def initialize_plex_server(plex_url: str, plex_token: str) -> PlexServer:
@@ -47,6 +47,7 @@ def initialize_plex_server(plex_url: str, plex_token: str) -> PlexServer:
         print(f"Error initializing Plex Server: {e}")
         exit()
 
+
 def get_music_library(plex: PlexServer, section_title: str):
     try:
         return plex.library.section(section_title)
@@ -54,26 +55,38 @@ def get_music_library(plex: PlexServer, section_title: str):
         print(f"Error accessing section by Title: {e}")
         exit()
 
+
 def read_songs_from_file(filename: str) -> list:
     try:
-        with open(filename, 'r') as file:
+        with open(filename, 'r', encoding='utf-8') as file:
             return [line.strip() for line in file if ' - ' in line]
     except FileNotFoundError:
         print(f"Error: '{filename}' not found.")
         exit()
 
+
 def simplify_string(input_string: str) -> str:
-    input_string = input_string.lower()  # Convert string to lowercase
-    input_string = re.sub(r"[^\w\s]", "", input_string)  # Remove all non-word characters
-    input_string = unidecode(input_string)  # Remove accents
+    input_string = input_string.lower()
+    input_string = re.sub(r"[^\w\s]", "", input_string)
+    input_string = unidecode(input_string)
     return input_string
 
+
+def alternate_name_variation(artist_name: str) -> str:
+    if " and " in artist_name:
+        return artist_name.replace(" and ", " & ")
+    elif " & " in artist_name:
+        return artist_name.replace(" & ", " and ")
+    else:
+        return artist_name
+        
+        
 def find_track_in_library(music_library, artist_name: str, track_title: str):
     artist_search = music_library.search(title=artist_name)
     for artist in artist_search:
         simplified_input_artist = simplify_string(artist_name)
         simplified_library_artist = simplify_string(artist.title)
-        if fuzz.ratio(simplified_input_artist, simplified_library_artist) > FUZZ_AMT:  # Threshold can be adjusted
+        if fuzz.ratio(simplified_input_artist, simplified_library_artist) > FUZZ_AMT:
             for album in artist.albums():
                 date_pattern = r'\d{4}[-\u2013\u2014\u2212/]\d{2}[-\u2013\u2014\u2212/]\d{2}|\d{2}[-\u2013\u2014\u2212/]\d{2}[-\u2013\u2014\u2212/]\d{4}'
                 exclude_pattern = r'\b(?:live|concert|sbd)\b'
@@ -89,9 +102,10 @@ def find_track_in_library(music_library, artist_name: str, track_title: str):
                         simplified_input_title = simplify_string(track_title)
                         simplified_library_title = simplify_string(track.title)
                         
-                        if fuzz.ratio(simplified_input_title, simplified_library_title) > FUZZ_AMT:  # Threshold can be adjusted
+                        if fuzz.ratio(simplified_input_title, simplified_library_title) > FUZZ_AMT:
                             return track
     return None
+
 
 def search_tidal(plex_token: str, artist: str, title: str):
     date_pattern = r'\d{4}[-\u2013\u2014\u2212/]\d{2}[-\u2013\u2014\u2212/]\d{2}|\d{2}[-\u2013\u2014\u2212/]\d{2}[-\u2013\u2014\u2212/]\d{4}'
@@ -100,6 +114,7 @@ def search_tidal(plex_token: str, artist: str, title: str):
     url = f"https://music.provider.plex.tv/hubs/search?query={quote(query)}&X-Plex-Token={plex_token}"
 
     response = requests.get(url)
+    print(url)
     if response.status_code != 200:
         print(f"Error: Unable to search Tidal: {response.status_code}")
         return None
@@ -131,12 +146,11 @@ def search_tidal(plex_token: str, artist: str, title: str):
                     return tidal_id
     return None
 
+
 def add_track_to_playlist(plex_url: str, plex_token: str, tidal_ids: list, playlist_ratingKey: str):
     if not tidal_ids:
         print("Cannot add tracks to a playlist with no Tidal IDs.")
         return
-    
-    BATCH_SIZE = 20  # or another appropriate batch size
     
     for i in range(0, len(tidal_ids), BATCH_SIZE):
         batch_ids = tidal_ids[i:i + BATCH_SIZE]
@@ -146,16 +160,19 @@ def add_track_to_playlist(plex_url: str, plex_token: str, tidal_ids: list, playl
         response = requests.put(add_to_playlist_url, headers={'X-Plex-Token': plex_token})
         
         if response.status_code == 200:
-            print(f"Tidal tracks added to the playlist successfully in batch {i//BATCH_SIZE + 1}.")
+            print(f"") ## (f"Tidal tracks added to the playlist successfully in batch {i//BATCH_SIZE + 1}.")
         else:
             print(f"Server says adding Tidal tracks to playlist in batch {i//BATCH_SIZE + 1}: {response.status_code}")
             print(response.text)
 
-def main():
-    # Initialize Plex Server with admin token
-    plex = PlexServer(PLEX_URL, PLEX_TOKEN)
 
-    # Proceeding with the rest of the logic
+
+
+def main():
+    plex = PlexServer(PLEX_URL, PLEX_TOKEN)
+    
+    
+    
     music_library = get_music_library(plex, SECTION_TITLE)
     songs = read_songs_from_file(FILENAME)
     playlist_name = input("Please enter the name for the new playlist: ")
@@ -163,31 +180,55 @@ def main():
     tidal_ids = []
 
     for song_name in songs:
-        artist, title = song_name.split(' - ')
+        print("-" * 10)
+        artist, title = song_name.split(' - ', 1)
+        print(f"Processing {artist} - {title}")
         local_track = find_track_in_library(music_library, artist, title)
         if local_track:
-            items.append(local_track)
-            print(f"Adding to playlist (Local): Artist: {artist}, Title: {title}, Track Details: {local_track}")
+            print("Found in local library.")
         else:
+            print("Not found in local library, searching on Tidal...")
             tidal_id = search_tidal(PLEX_TOKEN, artist, title)
             if tidal_id:
+                print("Found on Tidal")
                 tidal_ids.append(tidal_id)
-                print(f"Adding to playlist (Tidal): Artist: {artist}, Title: {title}, Tidal ID: {tidal_id}")
             else:
-                print(f"No local or Tidal track found for '{artist} - {title}'.")
+                print("Not found on Tidal")
 
-    if items or tidal_ids:
-        try:
-            if items:
-                local_playlist = plex.createPlaylist(playlist_name, items=items)
-                local_playlist_ratingKey = local_playlist.ratingKey
-                print(f"Local tracks added to the '{playlist_name}' playlist successfully.")
-            if tidal_ids:
-                add_track_to_playlist(PLEX_URL, PLEX_TOKEN, tidal_ids, local_playlist_ratingKey)
-        except Exception as e:
-            print(f"Error creating the playlist: {e}")
-    else:
-        print("No songs found to create a playlist.")
+    placeholder_track = find_track_in_library(music_library, PLACEHOLDER_ARTIST, PLACEHOLDER_TITLE)
+    if not placeholder_track:
+        print("Placeholder track not found.")
+        return
+
+    try:
+        local_playlist = plex.createPlaylist(playlist_name, items=[placeholder_track])
+        playlist_ratingKey = local_playlist.ratingKey
+
+        local_count = len(items)
+        
+        tidal_count = len(tidal_ids)
+        batch_count = 0
+
+        for track in items:
+            add_track_to_playlist(PLEX_URL, PLEX_TOKEN, [track.ratingKey], playlist_ratingKey)
+        for i in range(0, len(tidal_ids), BATCH_SIZE):
+            batch_ids = tidal_ids[i:i + BATCH_SIZE]
+            add_track_to_playlist(PLEX_URL, PLEX_TOKEN, batch_ids, playlist_ratingKey)
+            batch_count += 1
+
+        local_playlist.removeItems([placeholder_track])
+        # Summary output
+        print("\nPlaylist Creation Summary:")
+        print("-" * 30)  # Separator line for visual clarity
+        print(f"Playlist Name: {playlist_name}")
+        print(f"Local Tracks Added: {local_count}")
+        print(f"Tidal Tracks Added: {tidal_count}")
+        print(f"Total Batches Processed: {batch_count}")
+        print("-" * 30)
+        print("The playlist has been created and tracks have been added successfully.")
+
+    except Exception as e:
+        print(f"Error creating the playlist or adding tracks: {e}")
 
 if __name__ == "__main__":
     main()
