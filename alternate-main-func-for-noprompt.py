d3caae1e3b150db1e5d7c6cdd4811dda45023feb
bengarland <46571303+bengarland@users.noreply.github.com>
2023-11-14 11:28:30 -0700

    Update alternate-main-func-for-noprompt.py
    Improved colorized output and added a progress bar.

--- alternate-main-func-for-noprompt.py
+++ alternate-main-func-for-noprompt.py
@@ -4,38 +4,65 @@
 # in the original script.
 
 def main():
-    plex = PlexServer(PLEX_URL, PLEX_TOKEN)
-    
-    
-    
-    
+    plex = initialize_plex_server(PLEX_URL, PLEX_TOKEN)
+
+
+
+
     FILENAME = input("Input filename (or Enter for 'playlist.txt'): ").strip()
     if not FILENAME:
         FILENAME = 'playlist.txt'
 
     music_library = get_music_library(plex, SECTION_TITLE)
     songs = read_songs_from_file(FILENAME)
+
+    existing_playlists = [playlist.title for playlist in plex.playlists()]
     playlist_name = input("Please enter the name for the new playlist: ")
+
+    while playlist_name in existing_playlists:
+        print(f"Playlist '{playlist_name}' already exists. Please choose a different name.")
+        playlist_name = input("Please enter the name for the new playlist: ")
+
     local_items = []
     tidal_ids = []
-
+    not_found_tracks = []
+    log_messages = []
+    pbar = tqdm(total=len(songs), desc="Processing songs")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     for song_name in songs:
-        print(Colors.BLUE + "-" * 10 + Colors.RESET)
         artist, title = song_name.split(' - ', 1)
-        print(f"Processing {artist} - {title}")
-        local_track = find_track_in_library(music_library, artist, title)
+        log_message = f"{Colors.YELLOW}------------------------------{Colors.RESET}\n{Colors.BLUE}Processing {artist} - {title}{Colors.RESET}\n"
 
+        local_track = find_track_in_library(music_library, artist, title)
         if local_track:
-            print(Colors.GREEN + f"Found in local library: {local_track.title}" + Colors.RESET)
+            log_message += f"{Colors.GREEN}Found in local library: {local_track.title}{Colors.RESET}\n\n"
             local_items.append(local_track)
         else:
-            tidal_id = search_tidal(PLEX_TOKEN, artist, title)
+            tidal_id, final_url = search_tidal(PLEX_TOKEN, artist, title)
             if tidal_id:
                 tidal_ids.append(tidal_id)
-                print(Colors.CYAN + f"Found on Tidal: {tidal_id}" + Colors.RESET)
+                log_message += f"{Colors.CYAN}Found on Tidal: {tidal_id}{Colors.RESET}\n\n"
             else:
-                print(Colors.RED + f"Not found on Tidal or local: {artist} - {title}" + Colors.RESET)
+                log_message += f"{Colors.RED}Not found on Tidal or local: {artist} - {title}{Colors.RESET}\n{final_url}\n"
 
+        formatted_message = format_log_message(log_message, total_lines=4)
+        tqdm.write(formatted_message)
+        pbar.update(1)
+
+    pbar.close()
+
+    
     try:
         if local_items:
             local_playlist = plex.createPlaylist(playlist_name, items=local_items)
@@ -50,6 +77,11 @@ def main():
         else:
             print(Colors.YELLOW + "No Tidal tracks to add." + Colors.RESET)
 
+        if not_found_tracks:
+            print("\nTracks not found:")
+            for track in not_found_tracks:
+                print(track)
+            
         # Summary output
         print(f"\n{Colors.MAGENTA}Playlist Creation Summary:{Colors.RESET}")
         print(f"{Colors.YELLOW}-" * 30)  # Separator line for visual clarity
@@ -63,6 +95,10 @@ def main():
     except Exception as e:
         print(f"{Colors.RED}Error creating the playlist or adding tracks: {e}{Colors.RESET}")
 
+    if not_found_tracks:
+        print("\nTracks not found:")
+        for track in not_found_tracks:
+            print(track)
 
 if __name__ == "__main__":
     main()
